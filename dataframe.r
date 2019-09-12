4e64872f2c97ea8b1162d060b82b44d180349f7b
Rudresh Sisode <rudranrajput@gmail.com>
2019-09-12 08:21:21 +0530

    R Project File

---
+++ dataframe.r
@@ -0,0 +1,8 @@
+emp.data<-data.frame(
+  emp_id=c(1:5),
+  emp_name=c("Rudresh","soham","avinash","Prafulla","Avantika"),
+  salary=c(653,235,532,675,213),
+  start_date=as.Date(c("2012-01-02","2012-01-02","2012-01-02","2012-01-02","2012-01-02")),
+  stringsAsFactors = F)
+
+print(emp.data)
